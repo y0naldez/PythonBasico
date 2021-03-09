55dc02fb7b25efd026e206fc57ede2fbd923f085
Yonathan Hernández <yonaldez0@gmail.com>
2021-03-08 20:20:14 -0600

    Hoja de trabajo 2
    triangulo,cuenta atrás,primos

---
+++ Hoja1.py
@@ -0,0 +1,24 @@
+#Ejercicio 1
+
+n = int(input("Intoduzca el numero para el triangulo rectangulo: "))
+for i in range(n+1):
+    print("*"*i)
+
+
+#Ejercicio 2
+n = int(input("Intoduzca el numero para la cuenta regresiva: "))
+for i in range(n,-1,-1):
+    print(i,end=",")
+
+ #Ejercicio 3
+r= True
+n = int(input("Intoduzca el numero para comprobar si es primo o no: "))  
+d=0
+while d<n and r:
+    if n%2==0:
+        r=False
+    d=d+1
+if (r):
+    print("El numero es primo")
+else:
+    print("El numero no es primo")
